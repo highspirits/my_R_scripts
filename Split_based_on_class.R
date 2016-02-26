5306a70da44e0abac8540c97dac3d57e81cf6e0b
Venkata Sreedhar Nalam <sreedharnalam@gmail.com>
2016-02-25 23:40:17 -0500

    Update Split_based_on_class.R

--- Split_based_on_class.R
+++ Split_based_on_class.R
@@ -1,4 +1,5 @@
 #this function splits a dataframe based on the class of its columns
+#my.split.boc = my.split.based_on_class function
 
 my.split.boc = function(df) {
   return((split(df, sapply(df, function(x) paste(class(x), collapse = " ")))))
