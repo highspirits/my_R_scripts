25d4bef8847ab52b88d09860d126432b9c6ff13b
Venkata Sreedhar Nalam <sreedharnalam@gmail.com>
2016-08-13 18:00:59 -0400

    Add files via upload

---
+++ read_all_files_in_folder.R
@@ -0,0 +1,6 @@
+
+#get the names of all the files in a folder having extension as ".csv" 
+filenames = list.files(path = "C:/Sreedhar/Data Science Competitions/Crowd Analytix/Predict fuel flow rate of airplanes during different phases of a flight/Data/CAX_Train_1", pattern = ".csv")
+
+#Read all the files from a given folder into a dataframe
+train1 = do.call(rbind, lapply(paste("C:/Sreedhar/Data Science Competitions/Crowd Analytix/Predict fuel flow rate of airplanes during different phases of a flight/Data/CAX_Train_1/", fileslist, sep = ''), read.csv))
